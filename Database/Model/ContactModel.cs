cf80390cfe64d2197ca716f435f52ba8a513fc32
haduynhat2002 <nhathdth2009032@gmail.com>
2021-11-26 10:36:46 +0700

    BaiThiCshap

---
+++ Database/Model/ContactModel.cs
@@ -0,0 +1,71 @@
+﻿using Database.Data;
+using Database.Entities;
+using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Model
+{
+    public class NoteModel
+    {
+        public NoteModel()
+        {
+            DatabaseMigration.UpdateDatabase();
+        }
+        public bool Save(Note note)
+        {
+            try
+            {
+                using (SqliteConnection cnn = new SqliteConnection($"Filename={DatabaseMigration._databasePath}"))
+                {
+                    cnn.Open();
+                    SqliteCommand command = new SqliteCommand("INSERT INTO notes (Name, PhoneNumber)" +
+                " values (@name, @phoneNumber)", cnn);                    
+                    command.Parameters.AddWithValue("@name", note.Name);                                       
+                    command.Parameters.AddWithValue("@phoneNumber", note.PhoneNumber);                                       
+                    command.ExecuteNonQuery();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        public List<Note> FindAll()
+        {
+            List<Note> result = new List<Note>();
+            try
+            {
+                // mo ket noi den data base
+                using (SqliteConnection cnn = new SqliteConnection($"Filename={DatabaseMigration._databasePath}"))
+                {
+                    cnn.Open();
+                    SqliteCommand command = new SqliteCommand("SELECT * FROM notes", cnn);
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        var name = reader.GetString(0);
+                        var phoneNumber = reader.GetString(1);                      
+                        var note = new Note()
+                        {
+                            Name = name,
+                            PhoneNumber = phoneNumber,                          
+                        };
+                        result.Add(note);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return result;
+        }
+
+    }
+}
---
+++ Database/Pages/NoteForm.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Database.Entities;
+using Database.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Database.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class NoteForm : Page
+    {
+        private ContactModel noteModel = new ContactModel();
+        public NoteForm()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var note = new Note()
+            {              
+                Name = txtName.Text,
+                PhoneNumber = txtPhoneNumber.Text,                
+            };
+            var result = noteModel.Save(note);
+            if (result)
+            {
+                ContentDialog contentDialog = new ContentDialog();
+                contentDialog.Title = "Action success";
+                contentDialog.Content = $"Add contact success";
+                contentDialog.PrimaryButtonText = "Okie";
+                contentDialog.ShowAsync();
+                this.Frame.Navigate(typeof(Pages.NoteList));
+            }
+            else
+            {
+                ContentDialog contentDialog = new ContentDialog();
+                contentDialog.Title = "Action fails";
+                contentDialog.Content = $"Add contact fails. Please your again";
+                contentDialog.PrimaryButtonText = "Okie";
+                contentDialog.ShowAsync();
+            }
+        }
+    }
+}
